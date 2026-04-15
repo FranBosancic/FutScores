5d5ca085cc5441fb37c92169692e45080f8052b8
Fran Bosančić <fran.bosancic@gmail.com>
2026-04-15 12:00:06 +0200

    Promjena layouta, nista posebno

---
+++ ProbaMala/ProbaMala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProbaMala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
